9870b6cd134e9c0a18780ab595cc939c67d0019f
Tim Evans <evanstde@gmail.com>
2023-02-14 16:38:23 +0000

    Update names to correspond with function
    Rename minHeap to dwayHeap and rename binheap.zig to dway_heap.zig

--- src/dway_heap.zig
+++ src/dway_heap.zig
@@ -2,12 +2,12 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const testing = std.testing;
 
-/// An implementation of a binary heap storing type S.dtype,
+/// An implementation of a d-ary heap storing type S.dtype,
 /// and using S.compare to check which of two elements should
 /// be higher in the heap, returning true if the first
 /// argument should be higher, and false if the second should
 /// be higher.
-pub fn minHeap(comptime S: struct {
+pub fn dwayHeap(comptime S: struct {
     dtype: type = i32,
     branch_factor: usize = 2,
     compare: fn (anytype, anytype) bool = base_comp,
@@ -21,8 +21,8 @@ pub fn minHeap(comptime S: struct {
 
         /// Initialise a binary heap backed by an existing array slice.
         /// Will alter the contents of the slice.
-        pub fn init(store: []S.dtype) !minHeap(S) {
-            var heap = minHeap(S){
+        pub fn init(store: []S.dtype) !dwayHeap(S) {
+            var heap = dwayHeap(S){
                 .count = store.len,
                 .store = store,
                 .allocator = null,
@@ -32,10 +32,10 @@ pub fn minHeap(comptime S: struct {
         }
 
         /// Initialise a binary heap backed by a heap-allocated array.
-        /// Must be freed with minHeap.destroy() when no longer needed.
-        pub fn create(capacity: usize, allocator: Allocator) !minHeap(S) {
+        /// Must be freed with dwayHeap.destroy() when no longer needed.
+        pub fn create(capacity: usize, allocator: Allocator) !dwayHeap(S) {
             var store = try allocator.alloc(S.dtype, capacity);
-            return minHeap(S){
+            return dwayHeap(S){
                 .count = 0,
                 .store = store,
                 .allocator = allocator,
@@ -45,7 +45,7 @@ pub fn minHeap(comptime S: struct {
         /// Copies the contents of an existing slice into the array slice
         /// backing the heap. Source slice must not be longer than the
         /// heap's store slice.
-        pub fn load_data(self: *minHeap(S), src: []const S.dtype) !void {
+        pub fn load_data(self: *dwayHeap(S), src: []const S.dtype) !void {
             if (src.len > self.store.len) {
                 return error.InsufficientCapacity;
             }
@@ -54,16 +54,16 @@ pub fn minHeap(comptime S: struct {
             try self.heapify();
         }
 
-        /// Frees the underlying array slice backing a heap-allocated minHeap.
+        /// Frees the underlying array slice backing a heap-allocated dwayHeap.
         /// Does nothing if created with init (lacking an allocator).
-        pub fn destroy(self: *minHeap(S)) void {
+        pub fn destroy(self: *dwayHeap(S)) void {
             if (self.allocator != null) {
                 self.allocator.?.free(self.store);
             }
         }
 
         /// Inserts an element into the heap
-        pub fn add_elem(self: *minHeap(S), new_elem: S.dtype) !void {
+        pub fn add_elem(self: *dwayHeap(S), new_elem: S.dtype) !void {
             self.count += 1;
             if (self.count >= self.store.len) {
                 self.count -= 1;
@@ -73,7 +73,7 @@ pub fn minHeap(comptime S: struct {
             try self.float_up(self.count - 1);
         }
 
-        pub fn pop_root(self: *minHeap(S)) !S.dtype {
+        pub fn pop_root(self: *dwayHeap(S)) !S.dtype {
             if (self.count == 0) {
                 return error.PopFromEmpty;
             }
@@ -86,7 +86,7 @@ pub fn minHeap(comptime S: struct {
             return ret;
         }
 
-        pub fn increase_key(self: *minHeap(S)) !void {
+        pub fn increase_key(self: *dwayHeap(S)) !void {
             for (self.store) |_, ix| {
                 if (try self.float_up(ix) == true) {
                     return;
@@ -94,12 +94,12 @@ pub fn minHeap(comptime S: struct {
             }
         }
 
-        fn get_parent(self: *minHeap(S), ix: usize) !usize {
+        fn get_parent(self: *dwayHeap(S), ix: usize) !usize {
             _ = self;
             return try (ix - 1) / branch_factor;
         }
 
-        fn get_child(self: *minHeap(S), ix: usize, n_child: usize) !usize {
+        fn get_child(self: *dwayHeap(S), ix: usize, n_child: usize) !usize {
             _ = self;
             if (n_child >= branch_factor) {
                 return error.InvalidChildNumber;
@@ -109,7 +109,7 @@ pub fn minHeap(comptime S: struct {
 
         /// Returns the index in the heap of the child of root_ix with the
         /// highest priority
-        fn get_max_child_ix(self: *minHeap(S), root_ix: usize) usize {
+        fn get_max_child_ix(self: *dwayHeap(S), root_ix: usize) usize {
             const first_child_ix = (root_ix * branch_factor) + 1;
             const last_child_ix = if ((root_ix + 1) * branch_factor < self.count)
                 (root_ix + 1) * branch_factor
@@ -129,12 +129,12 @@ pub fn minHeap(comptime S: struct {
         /// Returns true if the node at ix has no children
         /// (index of children is outside the range used
         /// by the heap)
-        fn is_childless(self: *minHeap(S), ix: usize) bool {
+        fn is_childless(self: *dwayHeap(S), ix: usize) bool {
             return (ix * branch_factor) + 1 >= self.count;
         }
 
         /// Sorts the heap, ensuring that the heap property is satisfied.
-        fn heapify(self: *minHeap(S)) !void {
+        fn heapify(self: *dwayHeap(S)) !void {
             var ix = self.count / branch_factor;
             while (ix > 0) {
                 ix -= 1;
@@ -144,7 +144,7 @@ pub fn minHeap(comptime S: struct {
 
         /// Checks if the element at index ix is smaller than its parent,
         /// swapping them if so
-        fn float_up(self: *minHeap(S), ix: usize) !void {
+        fn float_up(self: *dwayHeap(S), ix: usize) !void {
             if (ix >= self.count) {
                 return error.IndexOutOfRange;
             }
@@ -168,7 +168,7 @@ pub fn minHeap(comptime S: struct {
 
         /// check if the element at index ix is larger than its children,
         /// swapping them if so.
-        fn sink_down(self: *minHeap(S), root_ix: usize) !void {
+        fn sink_down(self: *dwayHeap(S), root_ix: usize) !void {
             if (root_ix >= self.count) {
                 return error.IndexOutOfRange;
             }
@@ -199,7 +199,7 @@ fn base_comp(A: anytype, B: @TypeOf(A)) bool {
 test "Sorting in an existing array slice" {
     const dtype: type = f32;
     var data = [_]dtype{ 12, 6, 18, 19, 13 };
-    var heap = try minHeap(.{ .dtype = dtype }).init(&data);
+    var heap = try dwayHeap(.{ .dtype = dtype }).init(&data);
     try testing.expectEqual(@as(usize, 5), heap.count);
     try testing.expectEqual(@as(dtype, 6), try heap.pop_root());
     try testing.expectEqual(@as(dtype, 12), try heap.pop_root());
@@ -213,7 +213,7 @@ test "Sorting in a heap-allocated array slice" {
     const allocator = std.testing.allocator;
     const dtype: type = i32;
     const data = [_]dtype{ 12, 6, 18, 19, 13 };
-    var heap = try minHeap(.{}).create(16, allocator);
+    var heap = try dwayHeap(.{}).create(16, allocator);
     defer heap.destroy();
     try heap.load_data(&data);
     try testing.expectEqual(@as(usize, 5), heap.count);
@@ -228,7 +228,7 @@ test "Sorting in a heap-allocated array slice" {
 test "3-way heap in an existing array slice" {
     const dtype: type = f32;
     var data = [_]dtype{ 12, 6, 18, 19, 13 };
-    var heap = try minHeap(.{ .branch_factor = 3, .dtype = dtype }).init(&data);
+    var heap = try dwayHeap(.{ .branch_factor = 3, .dtype = dtype }).init(&data);
     try testing.expectEqual(@as(usize, 5), heap.count);
     try testing.expectEqual(@as(dtype, 6), try heap.pop_root());
     try testing.expectEqual(@as(dtype, 12), try heap.pop_root());
@@ -242,7 +242,7 @@ test "3-way heap in a heap-allocated array slice" {
     const allocator = std.testing.allocator;
     const dtype: type = i32;
     const data = [_]dtype{ 12, 6, 18, 19, 13 };
-    var heap = try minHeap(.{ .branch_factor = 3 }).create(16, allocator);
+    var heap = try dwayHeap(.{ .branch_factor = 3 }).create(16, allocator);
     defer heap.destroy();
     try heap.load_data(&data);
     try testing.expectEqual(@as(usize, 5), heap.count);
