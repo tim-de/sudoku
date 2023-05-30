b4ea893d04983a8a50eedf20d3c719f8c606f14f
Tim Evans <evanstde@gmail.com>
2023-05-30 19:46:41 +0100

    Tries to solve a sudoku
    Contains some code using the basic contents
    of sudoku.zig to attempt to solve a puzzle.
    At time of commit, also utterly fails...

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,19 @@
 const std = @import("std");
-const dwayHeap = @import("dway_heap.zig").dwayHeap;
+const SudokuGrid = @import("sudoku.zig").SudokuGrid;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 
 pub fn main() !void {
-    std.debug.print("Yeah, I've not done anything here yet...\n", .{});
+    const stdout = std.io.getStdOut().writer();
+    const arr = [_]u8{ 0, 0, 5, 0, 4, 0, 0, 0, 7, 0, 7, 0, 0, 0, 6, 0, 0, 0, 0, 9, 3, 0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 9, 0, 0, 0, 8, 0, 0, 0, 6, 0, 8, 0, 0, 0, 1, 0, 0, 0, 7, 0, 0, 0, 3, 0, 0, 4, 0, 0, 0, 9, 3, 0, 0, 0, 0, 8, 0, 0, 0, 6, 0, 7, 0, 0, 0, 1, 0, 5, 0, 0 };
+    // Try and solve a grid
+    var grid = try SudokuGrid.fromArray(&arr, allocator);
+    defer grid.cleanUp(allocator);
+    try grid.draw();
+    if (try grid.depthFirstSearch(allocator)) {
+        try stdout.print("Success!\n", .{});
+        try grid.draw();
+    } else {
+        try stdout.print("Failure :(\n", .{});
+    }
 }
